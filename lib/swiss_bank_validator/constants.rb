80a19609236da74b4fa5e2c41f2d6108cce03ab7
Kevin Robatel <kevin.robatel@qoqa.com>
2025-03-19 10:41:10 +0100

    Update libs

--- lib/swiss_bank_validator/constants.rb
+++ lib/swiss_bank_validator/constants.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 module SwissBankValidator
-  IBAN_REGEX = /\ACH[A-Z0-9]{2}\d{5}[A-Z0-9]{12}\z/.freeze
+  IBAN_REGEX = /\ACH[A-Z0-9]{2}\d{5}[A-Z0-9]{12}\z/
   IBAN_LENGTH = 21
 
   # Regex that allow only field accepted by Swiss bank to initiate a payment
   BANK_REGEX = %r{\A[a-zA-Z0-9.,;:’+\-()?*\[\]{}`´~'\s!“#%÷=@_$£
-                     àáâäçèéêëìíîïñòóôöùúûüýßÀÁÂÄÇÈÉÊËÌÍÎÏÒÓÔÖÙÚÛÜÑ]*\z}x.freeze
+                     àáâäçèéêëìíîïñòóôöùúûüýßÀÁÂÄÇÈÉÊËÌÍÎÏÒÓÔÖÙÚÛÜÑ]*\z}x
 end
--- swiss_bank_validator.gemspec
+++ swiss_bank_validator.gemspec
@@ -12,7 +12,7 @@ Gem::Specification.new do |spec|
   spec.summary       = 'All you need to validate a Swiss Iban'
   spec.homepage      = 'https://github.com/qoqa/swiss_bank_validator'
   spec.license       = 'MIT'
-  spec.required_ruby_version = Gem::Requirement.new('>= 2.7.0')
+  spec.required_ruby_version = Gem::Requirement.new('>= 3.1')
 
   spec.metadata['homepage_uri'] = spec.homepage
   spec.metadata['source_code_uri'] = 'https://github.com/qoqa/swiss_bank_validator'
